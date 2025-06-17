ade9c316e9246426990da44fb1fec73c6a87960d
Afiq <muhdafiq197@gmail.com>
2025-06-17 09:10:30 +0800

    Setup Shadui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
            */
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
-        <body className={`$(inter.variable) font-sans`}>
+        <body className={`$(inter.variable) dark font-sans`}>
           <div className="grid-rows-[auto, 1fr] grid h-screen">
             <TopNav />
             <main className="overflow-y-scroll">{children}</main>
